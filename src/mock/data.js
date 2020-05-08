7abe332554524f5a89197c1c390d8d4c79528748
Brandon Olin <olin.brandon@gmail.com>
2020-05-08 08:07:25 -0400

    'portfolio-finished'

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,7 +2,7 @@ import uuidv1 from 'uuid/v1';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
+  title: 'Brandon Olin | JavaScript Developer', // e.g: 'Name | Developer'
   lang: '', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
@@ -10,48 +10,54 @@ export const headData = {
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
+  name: 'Brandon Olin',
   subtitle: '',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'profile.png',
+  paragraphOne:
+    'Born in the frigid tundra of Buffalo, NY, the long winters indoors left plenty of time for fiddling with technology. I showed a knack for computers and spent much of my early years building gaming rigs and learning their ins and outs.',
+  paragraphTwo:
+    'I ended up pursuing a business degree and working various jobs in the field, eventually going down the path of entrepreneurship and location independence. This culminated in 3 years spent living and working remotely in South America, Europe, and Asia.',
+  paragraphThree:
+    'Now that I’ve finished the Le Wagon Ruby on Rails coding bootcamp I’m transitioning to programming as my full-time profession. I’m looking for positions anywhere in the U.S where I can make that happen!',
+  resume: 'https://drive.google.com/open?id=1gCqp4YuhbTDPdICXZP0ml1h4Q6mwO2WJ', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'smartbrain.png',
+    title: 'Smartbrain',
+    info:
+      'Full stack React + Node.js + PostgreSQL app, built solo as the final exercise in the Zero to Mastery full-stack web developer course.',
+    info2:
+      'Uses Clarifai machine learning AI to detect faces in images, and authorizes users while tracking the number of entries they submit.',
+    url: 'https://smartbrain-js.herokuapp.com/',
+    repo: 'https://github.com/brandon-olin/smartbrain', // if no repo, the button will not show up
   },
   {
     id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'sustainable.png',
+    title: 'SustainABLE',
+    info:
+      'Job board style app built with a team of three other developers in the 8th and 9th weeks of the Le Wagon coding bootcamp',
+    info2: 'Primarily responsible for the UX/UI and front end design.',
+    url: 'http://www.sustainable-app.com/',
+    repo: 'https://github.com/kathrin298/sustainABLE', // if no repo, the button will not show up
   },
   {
     id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'animal_kingdom.png',
+    title: 'Animal Kingdom',
+    info: 'Marketplace style app built with the same team in the 7th week of the Le Wagon bootcamp',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://animal-kingdom-kathrin298.herokuapp.com/',
+    repo: 'https://github.com/kathrin298/animal_kingdom', // if no repo, the button will not show up
   },
 ];
 
@@ -59,7 +65,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'brandon@brandonolin.com',
 };
 
 // FOOTER DATA
@@ -68,22 +74,22 @@ export const footerData = {
     {
       id: uuidv1(),
       name: 'twitter',
-      url: '',
-    },
-    {
-      id: uuidv1(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: uuidv1(),
-      name: 'linkedin',
-      url: '',
+      url: 'https://twitter.com/BrandonOlin',
     },
     {
       id: uuidv1(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/brandon-olin',
+    },
+    {
+      id: uuidv1(),
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/brandon-olin/',
+    },
+    {
+      id: uuidv1(),
+      name: 'codepen',
+      url: 'https://codepen.io/brandon-olin',
     },
   ],
 };
